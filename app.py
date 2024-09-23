5596c0925e51a2f72a316c2ad9093885a2c9c5c9
Samuel Santos <samuka0102128@gmail.com>
2024-09-22 21:35:11 -0300

    Create Method

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 #Como criar aplicação:
 #__name__ = "Main"
 app  = Flask(__name__)
 
 
-#Criar uma rota
+tasks = []
+task_id_control = 1
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
-
-@app.route("/About")
-def teste():
-    return "Página Sobre!"
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control 
+    data = request.get_json()
+    new_task = Task(id=task_id_control,title=data["title"], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
 
 if __name__ == "__main__": #Garantir que só vai rodar quando executarmos ele de forma manual
 #Como rodar
